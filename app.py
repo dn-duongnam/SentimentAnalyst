c7667f1a76a40f24c9bfba7f00835e5f2f841a1b
Gfnlydsa <dthaison02@gmail.com>
2023-12-14 22:08:03 +0700

    Merge test

--- app.py
+++ app.py
@@ -1,51 +1,68 @@
 from flask import Flask, render_template, request, jsonify
 import torch
 from transformers import AutoTokenizer, AutoModel
+from pyvi import ViTokenizer
+from joblib import load
 
 app = Flask(__name__)
-
-# Load pre-trained model
-model_path = './model/phobert/phobert_fold4.pth'
-tokenizer = AutoTokenizer.from_pretrained(
-    "vinai/phobert-base", use_fast=False, cache_dir="./model/phobert")
-Model = AutoModel.from_pretrained(
-    "vinai/phobert-base", cache_dir="./model/phobert")
-
-
-class SentimentClassifier(torch.nn.Module):
-    def __init__(self, n_classes):
-        super(SentimentClassifier, self).__init__()
-        self.bert = Model
-        self.drop = torch.nn.Dropout(p=0.3)
-        self.fc = torch.nn.Linear(self.bert.config.hidden_size, n_classes)
-        torch.nn.init.normal_(self.fc.weight, std=0.02)
-        torch.nn.init.normal_(self.fc.bias, 0)
-
-    def forward(self, input_ids, attention_mask):
-        last_hidden_state, output = self.bert(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            return_dict=False
-        )
-        x = self.drop(output)
-        x = self.fc(x)
-        return x
-
-
-# Load model weights
-model = SentimentClassifier(n_classes=3)  # Assuming 3 classes for sentiment
-model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-model.eval()
 
 
 @app.route("/bert", methods=["GET", "POST"])
 def bert():
+    # Load pre-trained model
+    model_path = './model/phobert/phobert_fold4.pth'
+    tokenizer = AutoTokenizer.from_pretrained(
+        "vinai/phobert-base", use_fast=False, cache_dir="./model/phobert")
+    Model = AutoModel.from_pretrained(
+        "vinai/phobert-base", cache_dir="./model/phobert")
+
+    class SentimentClassifier(torch.nn.Module):
+        def __init__(self, n_classes):
+            super(SentimentClassifier, self).__init__()
+            self.bert = Model
+            self.drop = torch.nn.Dropout(p=0.3)
+            self.fc = torch.nn.Linear(self.bert.config.hidden_size, n_classes)
+            torch.nn.init.normal_(self.fc.weight, std=0.02)
+            torch.nn.init.normal_(self.fc.bias, 0)
+
+        def forward(self, input_ids, attention_mask):
+            last_hidden_state, output = self.bert(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                return_dict=False
+            )
+            x = self.drop(output)
+            x = self.fc(x)
+            return x
+
+    # Load model weights
+    # Assuming 3 classes for sentiment
+    model = SentimentClassifier(n_classes=3)
+    model.load_state_dict(torch.load(
+        model_path, map_location=torch.device('cpu')))
+    model.eval()
+
     if request.method == "POST":
         comment = request.form["comment"]
         if comment:
             predicted_label = infer(comment, model, tokenizer)
             return jsonify({"predicted_label": predicted_label})
     return render_template("bert.html")
+
+
+@app.route("/svm", methods=["GET", "POST"])
+def svm():
+    vectorizer = load('./model/svm/tfidf_vectorizer.joblib')
+    svm_model = load('./model/svm/svm_model.joblib')
+    if request.method == "POST":
+        comment = request.form["comment"]
+        if comment:
+            comment = ViTokenizer.tokenize(comment)
+            comment = vectorizer.transform([comment])
+            predicted_label = convert_label_to_sentiment(
+                svm_model.predict(comment)[0])
+            return jsonify({"predicted_label": predicted_label})
+    return render_template("svm.html")
 
 
 def infer(text, model, tokenizer):
